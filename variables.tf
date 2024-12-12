ea585d38d35cc35a3a4279badcffeb95efa9fbf0
Mike Deeks <mike@braintrustdata.com>
2024-12-12 11:37:55 -0800

    Add types to all vars

--- variables.tf
+++ variables.tf
@@ -13,63 +13,74 @@ locals {
 }
 
 variable "deployment_name" {
+  type        = string
   default     = "braintrust"
   description = "Name of this Braintrust deployment. Will be included in tags and prefixes in resources names"
 }
 
 ## NETWORKING
 variable "vpc_cidr" {
+  type        = string
   default     = "172.29.0.0/16"
   description = "CIDR block for the VPC"
 }
 
 variable "private_subnet_1_az" {
+  type        = string
   default     = null
   description = "Availability zone for the first private subnet. Leave blank to choose the first available zone"
 }
 
 variable "private_subnet_2_az" {
+  type        = string
   default     = null
   description = "Availability zone for the first private subnet. Leave blank to choose the second available zone"
 }
 
 variable "private_subnet_3_az" {
+  type        = string
   default     = null
   description = "Availability zone for the third private subnet. Leave blank to choose the third available zone"
 }
 
 variable "public_subnet_1_az" {
+  type        = string
   default     = null
   description = "Availability zone for the public subnet. Leave blank to choose the first available zone"
 }
 
 variable "enable_quarantine" {
-  description = "Enable optional Quarantine VPC. If enabled, user defined functions run inside of this VPC."
   type        = bool
+  description = "Enable optional Quarantine VPC. If enabled, user defined functions run inside of this VPC."
   default     = false
 }
 
 variable "quarantine_vpc_cidr" {
+  type        = string
   default     = "172.30.0.0/16"
   description = "CIDR block for the Quarantined VPC"
 }
 
 variable "quarantine_private_subnet_1_az" {
+  type        = string
   default     = null
   description = "Availability zone for the first private subnet. Leave blank to choose the first available zone"
 }
 
 variable "quarantine_private_subnet_2_az" {
+  type        = string
   default     = null
   description = "Availability zone for the first private subnet. Leave blank to choose the second available zone"
 }
 
 variable "quarantine_private_subnet_3_az" {
+  type        = string
   default     = null
   description = "Availability zone for the third private subnet. Leave blank to choose the third available zone"
 }
 
 variable "quarantine_public_subnet_1_az" {
+  type        = string
   default     = null
   description = "Availability zone for the public subnet. Leave blank to choose the first available zone"
 }
