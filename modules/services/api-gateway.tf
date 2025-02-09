fb1073f3d87dd24d3886c314062aea061a2ce7bf
Mike Deeks <mike@braintrustdata.com>
2025-02-08 23:35:29 -0800

    Enable metrics

--- modules/services/api-gateway.tf
+++ modules/services/api-gateway.tf
@@ -19,6 +19,15 @@ resource "aws_api_gateway_stage" "api" {
   stage_name    = "api"
 }
 
+resource "aws_api_gateway_method_settings" "all" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.api.stage_name
+  method_path = "*/*"
+  settings {
+    metrics_enabled = true
+  }
+}
+
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
