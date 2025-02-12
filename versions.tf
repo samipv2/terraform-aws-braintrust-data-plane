10ef295ec8498c20311becbba6b221c26ac531d3
Mike Deeks <mike@braintrustdata.com>
2025-02-12 14:33:02 -0800

    Min version terraform 1.9

--- versions.tf
+++ versions.tf
@@ -1,5 +1,7 @@
 terraform {
-  required_version = ">= 1.0.0"
+  # v1.9 is needed for variable validation features
+  # v1.2 is needed for precondition checks
+  required_version = ">= 1.9.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
