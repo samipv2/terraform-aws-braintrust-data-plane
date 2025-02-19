d66eeb8113c386a2ea5ecf6cebc4276267632cc1
Mike Deeks <mike@braintrustdata.com>
2025-02-18 17:08:39 -0800

    Fix outputs and add clickhouse s3 bucket

--- examples/braintrust-data-plane/outputs.tf
+++ examples/braintrust-data-plane/outputs.tf
@@ -1,4 +1,8 @@
 output "api_url" {
-  value       = module.services.api_url
+  value       = module.braintrust-data-plane.api_url
   description = "The primary endpoint for the dataplane API. This is the value that should be entered into the braintrust dashboard under API URL."
+}
+output "clickhouse_s3_bucket_name" {
+  value       = module.braintrust-data-plane.clickhouse_s3_bucket_name
+  description = "Name of the Clickhouse S3 bucket"
 }
