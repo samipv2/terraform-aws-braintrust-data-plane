b6b08c3b3b1cb34d44d873f5dfb48780d1ccd2db
Mike Deeks <mike@braintrustdata.com>
2025-02-20 13:40:11 -0800

    Allow for providing an additional kms key policy

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -6,17 +6,20 @@ resource "aws_kms_key" "braintrust" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "Enable IAM User Permissions"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+    Statement = concat(
+      [
+        {
+          Sid    = "Enable IAM User Permissions"
+          Effect = "Allow"
+          Principal = {
+            AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+          }
+          Action   = "kms:*"
+          Resource = "*"
         }
-        Action   = "kms:*"
-        Resource = "*"
-      }
-    ]
+      ],
+      var.additional_key_policies
+    )
   })
 
   tags = {
