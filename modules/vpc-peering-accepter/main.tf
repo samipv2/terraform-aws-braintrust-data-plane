4a5e60c45115b8952aadc731beab8f4aa086ce70
Mike Deeks <mike@braintrustdata.com>
2025-02-19 16:43:41 -0800

    Add vpc peering accepter submodule

---
+++ modules/vpc-peering-accepter/main.tf
@@ -0,0 +1,18 @@
+
+# Accept a VPC peering request from another VPC.
+resource "aws_vpc_peering_connection_accepter" "accepter" {
+  vpc_peering_connection_id = var.vpc_peering_connection_id
+  auto_accept               = true
+
+  tags = {
+    Name = "braintrust-peer-accepter"
+  }
+}
+
+# Add a route to the source route tables to allow communication with the destination VPC
+resource "aws_route" "requester_to_accepter" {
+  for_each                  = toset(var.source_route_table_ids)
+  route_table_id            = each.value
+  destination_cidr_block    = var.destination_cidr
+  vpc_peering_connection_id = var.vpc_peering_connection_id
+}
