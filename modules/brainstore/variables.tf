a32595d53d1e45d8511236f977f9eed95fd9c3bf
Mike Deeks <mike@braintrustdata.com>
2025-03-16 12:40:35 -0700

    Ensure graviton and license key

--- modules/brainstore/variables.tf
+++ modules/brainstore/variables.tf
@@ -5,13 +5,22 @@ variable "deployment_name" {
 
 variable "instance_type" {
   type        = string
-  description = "The instance type to use for the Brainstore.Must be a Graviton instance type. Preferably with 16GB of memory and a local SSD for cache data. The default value is for tiny deployments. Recommended for production deployments is c7gd.8xlarge."
+  description = "The instance type to use for the Brainstore. Must be a Graviton instance type. Preferably with 16GB of memory and a local SSD for cache data. The default value is for tiny deployments. Recommended for production deployments is c7gd.8xlarge."
   default     = "c7gd.xlarge"
+
+  validation {
+    condition     = can(regex("[a-z0-9]*g[a-z0-9]*\\.", var.instance_type))
+    error_message = "Must be a Graviton instance type."
+  }
 }
 
 variable "license_key" {
   type        = string
   description = "The license key for the Brainstore"
+  validation {
+    condition     = length(var.license_key) > 0
+    error_message = "The license key cannot be empty."
+  }
 }
 
 variable "instance_count" {
